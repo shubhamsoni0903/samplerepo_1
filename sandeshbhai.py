904ea7a8cd6c47fd38d1c6d4f083d730d70f1da8
shubhamsoni0903 <shubham09.soni03@gmail.com>
2023-10-28 16:26:10 +0530

    For my first commit

---
+++ sandeshbhai.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+a = 50
+b = 60
+print (a+b)
+
+#if condition
+name = input("King :-")
+if name == 'Shubham':
+    print("king and Raja Of Bhopal")
+
+#IF - ELSE CONDITION
+name = input(' Enter The Name :- ')
+if name == 'Shubham':
+    print('King OF Bhopal')
+else:print('NEW KING IPS OFFICER DEEPANSHU')
+
+password = input('ENTER PASSWORD :-')
+if password == 'sameeksha':
+    print('unlock device')
+else:print('nikal bsdk')
